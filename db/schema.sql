d60b72575e44555d7f42cb046782b490eb46c3f5
John Stuart <31050027+johnstuart670@users.noreply.github.com>
2018-04-29 14:25:43 -0600

    corected the overlong name for a burger

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+create database burgers_db;
+
+create table burger_table (
+	ID INT NOT NULL auto_increment primary key,
+	burger_name VARCHAR(45) NOT NULL,
+	devoured boolean NOT NULL
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO burger_table (burger_name, devoured)
+VALUES 
+("Bacon Swiss Burger", false),
+("Avocado Veggie Burger", false),
+("Basic Cheeseburger", false),
+("Bleu Cheese Burger", false),
+("Nacho Burger", false),
+("Pastrami Super Burger", false),
+("Pastrami Regular Burger", false),
+("Signature Bacon Cheeseburger", false),
+("Breakfast Burger", false),
+("Standard Veggie Burger", false),
+("Cajun Fire Burger", false),
+("Turkey Burger with Corn", false),
+("Just Raw Meat", false),
+("Vegan Supreme", false),
+("Mac and Cheese Burger", false);
+
