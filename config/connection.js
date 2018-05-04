1623b4e151d0f64adfb6798b7e8723fe6ed00273
John Stuart <31050027+johnstuart670@users.noreply.github.com>
2018-05-03 18:13:11 -0600

    updated the connection to reference JawsDB instead of the localhost

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
+  host: "ocvwlym0zv3tcn68.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+  user: "ak12bwf2d3wojr42",
+  password: "pbm4fq77210edo4e",
+  database: "oukw5acp8oi4qqab"
 });
 
 connection.connect(function(err) {
